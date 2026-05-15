24b90271e555bb475b39d582ea06a33107120fa5
jackson2003-code <jvgc2@aluno.ifal.edu.br>
2026-05-14 21:14:53 -0300

    Add PassagemValorMonolitica class demonstrating value passing

---
+++ tarefa3-java-parametros/PassagemValorMonolitica.java
@@ -0,0 +1,22 @@
+public class PassagemValorMonolitica {
+
+    public static void alterarNumero(int x) {
+
+        System.out.println("Valor recebido no método: " + x);
+
+        x = 100;
+
+        System.out.println("Valor após alteração dentro do método: " + x);
+    }
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+
+        System.out.println("Valor antes da chamada: " + numero);
+
+        alterarNumero(numero);
+
+        System.out.println("Valor após a chamada: " + numero);
+    }
+}
